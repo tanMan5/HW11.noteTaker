c98c2ac75662e276ac33a2335350ecbb480a0231
Tanner Nielsen <tan-ali5@outlook.com>
2020-06-19 23:37:53 -0700

    trying to find problem with code, why wont node work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,8 +6,8 @@ const fs = require("fs");
 module.exports = function (app) {
     // GET API requests
 
-    app.get("/api/notes", function (req,res) {
-        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, jsonString) => {
+    app.get("/api/notes", function (req, res) {
+        fs.readFile(path.join(__dirname, "../db/db.json"), 'utf8', (err, jsonString) => {
             if (err) {
                 return console.log(err)
             }
@@ -16,40 +16,37 @@ module.exports = function (app) {
     });
 // should receive the new note and save request to body and then return the new note
     app.post("/api/notes", function (req, res) {
-      const note = req.body;
+        let note = req.body;
+        note.id = Math.random().toString(16).slice(2);
+        db.push(note);
+         let json = JSON.stringify(db);
 
-      note.id= Math.random().toString(16).slice(2);
-      db.push(note);
-      const json = JSON.stringify(db);
-
-
-        fs.writeFile(path.join(__dirname, "../db/db.json"), noteString, (err) => {
-                if (err) {
-                    return console.log(err);
-                }
-                console.log("Success");
-            });
-            res.json(req.body);
-        });
+        fs.writeFile(path.join(__dirname, "../db/db.json"), json, "utf8", (err) => {
+         if (err) {
+            console.log(err);
+        }
+    });
+    res.json(req.body);
+});
 
 
 
 // delete selected note
 app.delete("/api/notes/:id", function (req, res) {
-   const id = req.params.id;
-   for (i = 0; index < db.length; index++) {
-       const element = db[index];
-       if (element.id === id) {
-           db.splice(index, 1);
+    const id = req.params.id;
+    for (let index = 0; index < db.length; index++) {
+        const element = db[index];
+        if (element.id === id) {
+            db.splice(index, 1);
        };
    };
    const json = JSON.stringify(db);
 
 
-        fs.writeFile(path.join(___dirname, "../db/db.json"), json, "utf8", (err) => {
-            if (err) {
-                console.log(err);
-            }
+   fs.writeFile(path.join(__dirname, "../db/db.json"), json, "utf8", (err) => {
+    if (err) {
+        console.log(err);
+        }
     });
     res.json(db);
 });
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
