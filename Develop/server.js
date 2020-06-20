aadda937a2faa5e29697913556409eb7b786c32e
Tanner Nielsen <tan-ali5@outlook.com>
2020-06-19 22:10:53 -0700

    spelling mistake

--- Develop/server.js
+++ Develop/server.js
@@ -9,7 +9,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlendcoded({ extended: true}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 require("./routes/apiRoutes")(app);
