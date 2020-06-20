9d7c86f0b4e16884f1b0800ef33cae2b58513125
Tanner Nielsen <tan-ali5@outlook.com>
2020-06-19 23:15:18 -0700

    updated file organization

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+// Needs to include correct path package to get correct file path for the html
+
+const path = require("path");
+
+module.exports - function (app) {
+
+// GET REQUEST HTML
+// USER HTML 
+  app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    app.get("/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+};
+
